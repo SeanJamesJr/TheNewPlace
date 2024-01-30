7035982dd417de3b76aeb394406035c19e014009
sean <sean>
2024-01-30 10:51:06 -0500

    hi

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import pymysql
 from flask import Flask
+from flask import Flask, render_template
 
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+ return render_template("index.html.jinja")
